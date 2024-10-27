f7cccf9a49d94620df25b90460bc35f385055c93
Prakash Kushwaha <prakash.sde3@gmail.com>
2024-10-27 15:50:07 +0530

    initial commit

---
+++ avr-lm35/Debug/Include/MCAL/ADC_Program.d
@@ -0,0 +1,12 @@
+Include/MCAL/ADC_Program.d Include/MCAL/ADC_Program.o: \
+ ../Include/MCAL/ADC_Program.c ../Include/MCAL/../LIB/STD_TYPES.h \
+ ../Include/MCAL/../LIB/BIT_MATH.h ../Include/MCAL/ADC_Config.h \
+ ../Include/MCAL/ADC_Register.h
+
+../Include/MCAL/../LIB/STD_TYPES.h:
+
+../Include/MCAL/../LIB/BIT_MATH.h:
+
+../Include/MCAL/ADC_Config.h:
+
+../Include/MCAL/ADC_Register.h:
